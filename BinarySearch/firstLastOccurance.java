3284152c6bac167b3719fc08a90e1536878d8c89
Sufyan Ahmed <ahmed.sufyan1310@gmail.com>
2023-12-12 14:58:00 +0530

    added last occurance block

--- BinarySearch/firstLastOccurance.java
+++ BinarySearch/firstLastOccurance.java
@@ -1,18 +1,36 @@
 public class firstLastOccurance{
-public static void main(String [] args){
-	int nums[] = {1,1,2,2,2,3,3,3,4,4,4,5,5};
-	System.out.println(first(nums,3));
-    }
-public static void first(int[] a, int target) {
-        int start = 0, end = a.length-1;
+	public static void main(String [] args){
+		int nums[] = {1,1,2,2,2,3,3,3,4,4,4,5,5};
+		System.out.println(first(nums,3));
+		System.out.println(last(nums,3));
+    	}
+	public static void first(int[] a, int target) {
+        	int start = 0, end = a.length-1;
+        	while (start <= end) {
+            	int mid = start + (end - start)/2;
+            	if (a[mid] < target)
+                	start = mid + 1;
+            	else
+                	end = mid - 1;
+        	}
+        	System.out.println("The first occurance is "+start);
+    	}
+	public static void last(int [] a, int target)
+    {
+        int start = 0, end = a.length-1, last_occurance = 0;
         while (start <= end) {
             int mid = start + (end - start)/2;
-            if (a[mid] < target)
-                start = mid + 1;
-            else
+            if (a[mid] > target){
                 end = mid - 1;
+            }
+            else if (a[mid] < target)
+                start = mid + 1;
+            else {
+                start = mid + 1;
+                last_occurance = mid;
+            }
         }
-        System.out.println("The first occurance is "+start);
+        System.out.println("The last occurance of the  element is "+last_occurance);
     }
 }
 
