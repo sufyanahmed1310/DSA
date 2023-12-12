3f20892e9ca13022866c4350a70e02f75181b8b2
Sufyan Ahmed <107124365+sufyanahmed1310@users.noreply.github.com>
2023-12-12 14:52:22 +0530

    Update firstLastOccurance.java

--- BinarySearch/firstLastOccurance.java
+++ BinarySearch/firstLastOccurance.java
@@ -1,18 +1,18 @@
 public class firstLastOccurance{
-public static void main(String [] args){
-	int nums[] = {1,1,2,2,2,3,3,3,4,4,4,5,5};
-	System.out.println(first(nums,3));
-    }
-public static void first(int[] a, int target) {
-        int start = 0, end = a.length-1;
-        while (start <= end) {
-            int mid = start + (end - start)/2;
-            if (a[mid] < target)
-                start = mid + 1;
-            else
-                end = mid - 1;
-        }
-        System.out.println("The first occurance is "+start);
-    }
+	public static void main(String [] args){
+		int nums[] = {1,1,2,2,2,3,3,3,4,4,4,5,5};
+		System.out.println(first(nums,3));
+    	}
+	public static void first(int[] a, int target) {
+        	int start = 0, end = a.length-1;
+        	while (start <= end) {
+            	int mid = start + (end - start)/2;
+            	if (a[mid] < target)
+                	start = mid + 1;
+            	else
+                	end = mid - 1;
+        	}
+        	System.out.println("The first occurance is "+start);
+    	}
 }
 
